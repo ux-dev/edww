dcd3e640c1c12e9fcedec3779ad0552d79ff4cba
Jen <jen@Jens-MBP.home.loc>
2018-11-11 21:27:46 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var PATHS = {
+    js: './js',
+	sass: './_sass', 
+    css: './css'
+};
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    // grunt.loadTasks('grunt-custom-includes');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+       
+        //step 0
+        sass: { 
+            options: {
+                sourceMap: false,
+            },
+
+            main: {
+                files:[{
+                    cwd: PATHS.sass,
+                    src: ['*.scss'],
+                    dest: PATHS.css,
+                    expand: true,
+                    ext: '.css',
+                    // sourceComments: true    
+                }] 
+            },
+
+            // font_awesome: {
+               
+            // },
+
+            // bootstrap: {
+
+            // }
+        },
+
+        //step 3
+        concat: {
+            js: {
+                dest: PATHS.js + '/main.compiled.js',
+                src: PATHS.js 
+            },
+            css: {
+                dest: PATHS.css + '/main.compiled.css',
+                src: [
+                    PATHS.css + '/main.min.css',
+                    // PATHS.css + '/vendor/bootstrap.css',	
+                    // PATHS.css + '/vendor/font-awesome.css'
+               ]
+            }
+        },
+
+        // //step 2
+        // uglify: {
+        //     default: {
+        //         options: {
+        //             compress: true,
+        //             mangle: false,
+        //             beautify: true //set to false to minify files
+        //         },
+        //         files: {
+        //             src: PATHS.js + '/main.compiled.js',
+        //             dest: PATHS.js + '/main.min.js'
+        //         }  
+        //     }
+        // },
+        
+        //step 2
+        cssmin: {
+            main: {
+                options: {
+                    rebase: false,
+                    level: { 1: { specialComments: 'none' } }
+                },
+                files: [{
+                    src: PATHS.css + '/main.css',
+                    dest: PATHS.css + '/main.min.css'
+                }]
+            }
+        }
+    });
+     
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    
+    // Register the grunt build tasks
+    grunt.registerTask('default', ['build-css', 'build-js']);
+    grunt.registerTask('build-css', ['sass:main', 'cssmin:main']); 
+    // grunt.registerTask('build-css', ['sass:main', 'cssmin:main', 'concat: main']); 
+    grunt.registerTask('build-js', ['concat:js', 'uglify:js']); 
+
+};
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include_once("configs.php") ?>
+
+<!DOCTYPE html>
+    <head>
+        <title>[TITLE]</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="css/reset.css"/>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?Open+Sans:400,600,700">
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+        <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+
+        
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="js/public/main.js"></script>
+
+        <script>
+                $(document).ready(function(){
+                
+                });
+        </script>
+    </head>
+    <body>
+        <header></header>
+        <main></main>
+        <footer></footer>
+    </body>
+</html>
