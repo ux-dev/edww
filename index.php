8fd4f060b3510e991ad39b14154c0ba7486e83ff
Jen <jen@Jens-MBP.home.loc>
2018-11-18 13:01:56 -0500

    updated home to have hero and nav yellow rose as hero

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<?php include_once("configs.php") ?>
-
 <!DOCTYPE html>
     <head>
-        <title>[TITLE]</title>
+        <title>Esteemedhelga Artwork Design</title>
         
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" type="text/css" href="css/reset.css"/>
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?Open+Sans:400,600,700">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=EB+Garamond|Oswald">
         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
         <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
@@ -15,7 +13,7 @@
 
         
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="js/public/main.js"></script>
+        <!-- <script type="text/javascript" src="js/public/main.js"></script> -->
 
         <script>
                 $(document).ready(function(){
@@ -24,7 +22,23 @@
         </script>
     </head>
     <body>
-        <header></header>
+        <header>
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="#">Esteemedhelga</a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link">About Me</a></li>
+                <li class="nav-item"><a class="nav-link">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link">Contact</a></li>
+            </ul>
+            </nav>
+        </header>
+        <div class="hero-image">
+            <div class="hero-text">
+                <h1>Esteemedhelga </h1>
+                <h2>Artwork Design</h2>
+            <div>
+        </div>
+    
         <main></main>
         <footer></footer>
     </body>
